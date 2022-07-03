0f6434d7de77f164516ff00a60c86f50c7e46f28
Matheus Gimenez <gimenez.lopes@hotmail.com>
2022-07-03 15:57:43 -0300

    Add project files.

---
+++ Kafka_POC/Controllers/DroneController.cs
@@ -0,0 +1,59 @@
+﻿using GeoCoordinatePortable;
+using Kafka_POC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kafka_POC.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DroneController : ControllerBase
+    {
+        private readonly ILogger<DroneController> _logger;
+
+        public DroneController(ILogger<DroneController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult InserirDado(DroneDados droneDados)
+        {
+            try
+            {
+                _logger.LogInformation($"Dados do drone {droneDados.Drone_Id} inserido com sucesso!");
+
+                GeoCoordinate coordinate = new GeoCoordinate(droneDados.Latitude, droneDados.Longitude);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Erro ao inserir dados do Drone");
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("Lista")]
+        public IActionResult InserirListaDados(List<DroneDados> dados)
+        {
+            try
+            {
+                foreach(DroneDados droneDados in dados)
+                {
+                    _logger.LogInformation($"Dados do drone {droneDados.Drone_Id} inserido com sucesso!");
+
+                }
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Erro ao inserir lista de dados do Drone");
+
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ Kafka_POC/Models/DroneDados.cs
@@ -0,0 +1,15 @@
+﻿
+using GeoCoordinatePortable;
+
+namespace Kafka_POC.Models
+{
+    public class DroneDados
+    {
+        public int Drone_Id { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }   
+        public int Temperatura { get; set; }
+        public int Umidade { get; set; }
+        public bool RastreioAtivo {get;set;}
+    }
+}
